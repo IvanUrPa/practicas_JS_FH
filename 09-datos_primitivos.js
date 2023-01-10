b37b14827ccfdef54558aafe188d6a3d09f71c3d
Ivan Urbano <ivanurbano@htech.com.mx>
2023-01-10 17:07:11 -0600

    retomar curso

---
+++ 09-datos_primitivos.js
@@ -0,0 +1,19 @@
+let num = 10
+let str = "texto"
+let bol = true
+let und = undefined
+let nul = null
+
+console.log(num)
+
+let obj = {
+  numero: 10,
+  texto: "Nuevo Texto",
+
+  objHijo: {
+    numero2: 5,
+    texto2: "Nuevo texto 2"
+  }
+}
+
+console.log(obj)
---
+++ 10-valor_referencia.js
@@ -0,0 +1,24 @@
+let a = 10
+let b = a
+
+console.log("a: ", a)
+console.log("b: ", b)
+
+a = 20
+
+console.log("a: ", a)
+console.log("b: ", b)
+
+
+let c = {
+  nombre: "Juana"
+}
+let d = c
+
+console.log("c: ", c)
+console.log("d: ", d)
+
+c.nombre = "María"
+
+console.log("c: ", c)
+console.log("d: ", d)
